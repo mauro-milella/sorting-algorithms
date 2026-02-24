a9d7a84f407aa24ef2e34eddcab1e72d4d1dabcd
mauro-milella <milella_mauro@live.it>
2026-02-24 10:36:07 +0100

    [ADD] basic skeleton and config

---
+++ include/config.h
@@ -0,0 +1,23 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <stdint.h>
+
+#define NAME_BUF 64
+#define ALGO_BUF 16
+
+struct configuration {
+    char name[NAME_BUF];
+
+    char algorithms[ALGO_BUF][NAME_BUF];
+    uint8_t algorithm_count;
+
+    uint32_t repetitions;
+
+    uint64_t min_size;
+    uint64_t max_size;
+
+    uint64_t seed;
+};
+
+#endif 
