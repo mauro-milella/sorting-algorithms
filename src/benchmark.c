2972f4e6109afce6e245aa76a48ea52109047130
mauro-milella <milella_mauro@live.it>
2026-02-24 14:13:42 +0100

    [ADD] command line arguments are mandatory

--- src/benchmark.c
+++ src/benchmark.c
@@ -11,20 +11,13 @@ int main(int argc, char *argv[])
 {
     char conf_path[CONF_PATH_BUF];
 
-    // if the user provided a string, use it as the configuration relative path 
-    if (argc > 1) {
+    // the path of the configuration must be specified in the command line
+    if (argc == 2) {
         snprintf(conf_path, sizeof(conf_path), "%s", argv[1]);
-    } 
-    // otherwise, ask for it
+    }
     else {
-        printf("Enter the : ");
-        if (fgets(conf_path, sizeof(conf_path), stdin) == NULL) {
-            fprintf(stderr, "Error reading conf_filename.\n");
-            return EXIT_FAILURE;
-        }
-
-        // remove trailing newline
-        conf_path[strcspn(conf_path, "\n")] = '\0';
+        printf("Usage: ./name <configuration_filepath>");
+        return EXIT_FAILURE;
     }
 
     struct configuration configuration;
