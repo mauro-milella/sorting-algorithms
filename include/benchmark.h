ba0799e45a4bed09f8b3e4ccbc859a21c7abe92d
mauro-milella <milella_mauro@live.it>
2026-03-04 22:20:19 +0100

    [ADD] benchmarking logic

---
+++ include/benchmark.h
@@ -0,0 +1,31 @@
+#ifndef BENCHMARK_H 
+#define BENCHMARK_H
+
+#include <stdint.h>
+
+#include "config.h"
+
+
+struct benchmark_input {
+    void *data;
+    uint64_t size;
+};
+
+
+typedef void (*input_generator_ptr)(struct benchmark_input *, uint32_t);
+
+typedef void (*algorithm_ptr)(struct benchmark_input *);
+typedef algorithm_ptr (*algorithm_selector_ptr)(char *);
+
+typedef int (*test_ptr)(struct benchmark_input *);
+
+
+void benchmark(
+    struct configuration *config, 
+    input_generator_ptr input_generator,
+    algorithm_selector_ptr algorithm_selector,
+    test_ptr test_strategy
+);
+
+#endif 
+
---
+++ src/benchmark.c
@@ -0,0 +1,67 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+#include "benchmark.h"
+
+
+double run(
+    uint64_t size, 
+    uint32_t repetitions, 
+    input_generator_ptr input_generator,
+    algorithm_ptr algorithm,
+    test_ptr test_strategy
+) {
+    double elapsed_time = 0.0;
+
+    for (uint32_t i=0; i<repetitions; i++) {
+        struct benchmark_input binput;
+        input_generator(&binput, size);
+        
+        clock_t start, end;
+        start = clock();
+        algorithm(&binput);
+        end = clock();
+
+        if (test_strategy(&binput) != 1) {
+            printf("ERRORE: test fallito.\n");
+            exit(-1);
+        }
+
+        elapsed_time += (end-start) / (double)CLOCKS_PER_SEC;
+
+        free(binput.data);
+    }
+
+    return elapsed_time / repetitions;
+}
+
+
+void benchmark(
+    struct configuration *config, 
+    input_generator_ptr input_generator,
+    algorithm_selector_ptr algorithm_selector,
+    test_ptr test_strategy
+) {
+    uint32_t initial_seed = config->seed;
+
+    for (uint8_t algo_idx=0; algo_idx<config->algorithm_count; algo_idx++) {
+        uint32_t mutable_seed = initial_seed;
+
+        for (uint64_t i=config->min_size; i<=config->max_size; i+=config->step) {
+            srand(mutable_seed);
+
+            double elapsed_time = run(
+                i,
+                config->repetitions, 
+                input_generator,
+                algorithm_selector(config->algorithms[algo_idx]),
+                test_strategy
+            );
+
+            mutable_seed++;
+
+            printf("Elapsed time: %g", elapsed_time);
+        }
+    }
+}
