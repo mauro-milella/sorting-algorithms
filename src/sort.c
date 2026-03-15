e2e66758d6ecf5d0e2f4197cc636403e8a14ef9c
mauro-milella <milella_mauro@live.it>
2026-03-15 10:58:45 +0100

    [ADD] merge sort implementation

--- src/sort.c
+++ src/sort.c
@@ -27,6 +27,52 @@ void insertion_sort(struct benchmark_input *binput) {
 }
 
 
+void merge(int64_t *data, uint32_t start, uint32_t mid, uint32_t end) {
+    int n1 = mid - start + 1;
+    int n2 = end - mid;
+
+    int i, j, left[n1], right[n2];
+
+    for (i=0; i<n1; i++) {
+        left[i] = data[start+i];
+    }
+
+    for (j=0; j<n2; j++) {
+        right[j] = data[mid+1+j];
+    }
+
+    i = j = 0;
+
+    for (uint32_t k=start; k<=end; k++) {
+        if (i < n1) {
+            if (j < n2) {
+                data[k] = (left[i] <= right[j]) ? left[i++] : right[j++];
+            } else {
+                data[k] = left[i++];
+            }
+        } else {
+            data[k] = right[j++];
+        }
+    }
+}
+
+void _merge_sort(int64_t *data, uint32_t start, uint32_t end) {
+    if (start < end) {
+        uint32_t mid = start + (end-start) / 2;
+        _merge_sort(data, start, mid);
+        _merge_sort(data, mid+1, end);
+        merge(data, start, mid, end);
+    }
+}
+
+void merge_sort(struct benchmark_input *binput) {
+    int64_t *data = (int64_t *)binput->data;
+    uint32_t size = binput->size;
+
+    _merge_sort(data, 0, size-1);
+}
+
+
 void generate_array(
     struct benchmark_input *binput, 
     uint32_t size
@@ -45,6 +91,9 @@ void generate_array(
 algorithm_ptr select_sorting_algorithm(char *algo_name) {
     if (strcmp(algo_name, "INSERTION") == 0) {
         return &insertion_sort;
+    }
+    else if (strcmp(algo_name, "MERGE") == 0) {
+        return &merge_sort;
     }
     else {
         printf("Algoritmo %s non selezionato.", algo_name);
