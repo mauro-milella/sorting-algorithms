d9ef28264f7d352ba59426338ccd7f2dbb77c7a8
mauro-milella <milella_mauro@live.it>
2026-03-18 11:44:24 +0100

    [FIX] struct configuration rearrangements

--- include/config.h
+++ include/config.h
@@ -8,18 +8,16 @@
 
 struct configuration {
 	char name[NAME_BUF];
-
 	char algorithms[ALGO_BUF][NAME_BUF];
-	uint8_t algorithm_count;
-
-	uint8_t step;
-
-	uint32_t repetitions;
 
 	uint64_t min_size;
 	uint64_t max_size;
-
 	uint64_t seed;
+
+	uint32_t repetitions;
+
+	uint8_t algorithm_count;
+	uint8_t step;
 };
 
 void config_init(struct configuration* config);
